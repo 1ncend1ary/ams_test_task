b3e18a6154af2b3f127aa2d2d1f97f53a192ca6c
Alex <alexseliverstow@gmail.com>
2021-02-14 13:36:13 +0300

    Escape = symbols for markdown.

--- main.py
+++ main.py
@@ -35,14 +35,14 @@ Source code: [GitHub](https://github.com/1ncend1ary/ams_test_task)
 /start \- display this message
 /help \- get commands help
 /coords lat long \- get a map with marked location
-Format: 0 <= lat <= 100, 0 <= long <= 100
+Format: 0 <\= lat <\= 100, 0 <\= long <\= 100
 """
 
 commands_text = """
 /start \- display the start message
 /help \- get this help
 /coords lat long \- get a map with marked location
-Format: 0 <= lat <= 100, 0 <= long <= 100
+Format: 0 <\= lat <\= 100, 0 <\= long <\= 100
 """
 
 
