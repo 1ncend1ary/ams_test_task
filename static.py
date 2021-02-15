47c9a12f28090447296de73c55b81a521486e6a7
Alex <alexseliverstow@gmail.com>
2021-02-15 10:09:53 +0300

    Add info to help messages.

--- static.py
+++ static.py
@@ -5,14 +5,14 @@ Source code: [GitHub](https://github.com/1ncend1ary/ams_test_task)
 /start \- display this message
 /help \- get commands help
 /coords long lat \- get a map with marked location
-Format: 0 <\= long <\= 67, 0 <\= lat <\= 100
+Format: integers, 0 <\= long <\= 67, 0 <\= lat <\= 100
 """
 
 commands_text = """
 /start \- display the start message
 /help \- get this help
 /coords long lat \- get a map with marked location
-Format: 0 <\= long <\= 67, 0 <\= lat <\= 100
+Format: integers, 0 <\= long <\= 67, 0 <\= lat <\= 100
 """
 
 # this scale is caused by the map image
