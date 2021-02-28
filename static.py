dfada26c2c218a3e6210aec9a6c3d2a4ba3d3985
Aleksei Seliverstov <alexseliverstow@gmail.com>
2021-02-28 10:30:22 +0300

    Add escape sequences fro special characters.

--- static.py
+++ static.py
@@ -2,17 +2,17 @@ start_text = r"""
 Author: [incend1ary](https://t.me/incend1ary) [Aleksei Seliverstov]
 Source code: [GitHub](https://github.com/1ncend1ary/ams_test_task)
 
-/start - display this message
-/help - get commands help
-/coords long lat - get a map with marked location
-Format: integers, 60.211170 <= long <= 60.220899, 29.724268 <= lat <= 29.781255
+/start \- display this message
+/help \- get commands help
+/coords long lat \- get a map with marked location
+Format: integers, 60\.211170 <\= long <\= 60\.220899, 29\.724268 <\= lat <\= 29\.781255
 """
 
 commands_text = r"""
-/start - display the start message
-/help - get this help
-/coords long lat - get a map with marked location
-Format: integers, 60.211170 <= long <= 60.220899, 29.724268 <= lat <= 29.781255
+/start \- display the start message
+/help \- get this help
+/coords long lat \- get a map with marked location
+Format: integers, 60\.211170 <\= long <\= 60\.220899, 29\.724268 <\= lat <\= 29\.781255
 """
 
 # this scale is caused by the map image
