bbea12d2a7485ff4f9cb1c0920221312707667f8
Aleksei Seliverstov <alexseliverstow@gmail.com>
2021-02-28 11:39:45 +0300

    Add map handling when on point.

--- main.py
+++ main.py
@@ -78,6 +78,8 @@ def location(update: Update, context: CallbackContext):
     else:
         update.message.reply_text(f'You are not inside the map. '
                                   f'Your location however is: {user_location.longitude} {user_location.latitude}')
+        update.message.text = f'{user_location.latitude} {user_location.longitude}'
+        coords(update, context)
 
 
 def error(update, context):
